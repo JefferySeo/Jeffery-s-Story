5b7ac1570c46406d7e2ab7e6abf7eb9b80165ba5
JefferySeo <125331815+JefferySeo@users.noreply.github.com>
2023-04-03 00:30:25 +0900

    0402

--- js/script.js
+++ js/script.js
@@ -5,13 +5,20 @@ $(function(){
         }else{
            $('#scrollUp').fadeOut();
         }
+        if($(window).scrollTop()>100){
+            $('#header').addClass('header-act');
+        }else{
+            $('#header').removeClass('header-act');
+        }
      });
+     $('.skills').myskillbar({});
 });// jQ
 const header = document.getElementById('header');
-
+const introHeight = document.getElementById('intro').scrollHeight;
+const jeffHeight = document.getElementById('Jeffery').scrollHeight
 
 const trans = document.getElementsByClassName('trans');
-
+trans[0].classList.add('active');
 function transPage(n){
     for(let i = 0 ; i < trans.length ; i++){
         trans[i].classList.remove('active');
@@ -20,9 +27,25 @@ function transPage(n){
     switch(n){
         case 0 : window.scrollTo({top:0, behavior:'smooth'})
         break;
-        
+        case 1 : window.scrollTo({top:introHeight, behavior:'smooth' })
+        break;
+        case 2 : window.scrollTo({top:introHeight+jeffHeight , behavior:'smooth'})
     }
 }
+window.addEventListener('scroll', function(){
+    if(this.window.scrollY < introHeight-10){
+        for(let i = 0 ; i < trans.length ; i++){
+            trans[i].classList.remove('active');
+        };
+        trans[0].classList.add('active');
+    }
+    else if(this.window.scrollY >= introHeight-10){
+        for(let i = 0 ; i < trans.length ; i++){
+            trans[i].classList.remove('active');
+        };
+        trans[1].classList.add('active');
+    }
+})
 
 function scrollUp(){
     window.scrollTo({top:0, behavior: 'smooth'});
